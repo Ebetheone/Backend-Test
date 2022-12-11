a6a74dfddc6d679eac9056a108249498ea0bbb67
Elbeg <elbeg.db@yahoo.com>
2022-12-11 17:45:17 +0800

    feat: fix

--- src/models/Orlogo.ts
+++ src/models/Orlogo.ts
@@ -1,11 +1,16 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IOrlogo extends Document {
+  id: string;
   orlogo: Int32Array;
 }
 
 const orlogoSchema = new Schema({
   orlogo: {
+    type: String,
+    required: true,
+  },
+  id: {
     type: String,
     required: true,
   },
