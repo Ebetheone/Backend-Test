2661dfb9b53c3d76be15797dfd5af7faae2372b3
Elbeg <elbeg.db@yahoo.com>
2022-12-15 15:18:18 +0800

    feat: fix

--- src/models/Zarlaga.ts
+++ src/models/Zarlaga.ts
@@ -2,10 +2,20 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface IZarlaga extends Document {
   zarlaga: Int32Array;
+  date: Date;
+  detail: string;
 }
 
 const zarlagaSchema = new Schema({
   zarlaga: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  detail: {
     type: String,
     required: true,
   },
